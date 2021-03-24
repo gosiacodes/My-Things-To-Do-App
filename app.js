489b9a448955e014868b62720c65cf7e013da6fd
margareta75 <margareta.xx@gmail.com>
2021-03-24 22:33:16 +0100

    Changed code to send data, added code to delete all data from Firebase, testing fetching, code cleaning.
    New codepen: https://codepen.io/margareta75/pen/qBROZLW

--- app.js
+++ app.js
@@ -1,64 +1,56 @@
-// Disable buttons without functions
+// Web app's Firebase configuration.
+var firebaseConfig = {
+    apiKey: "AIzaSyB5kRc09ifanS9_tXPoOAPh2OYcYGZibM8",
+    authDomain: "my-things-to-do.firebaseapp.com",
+    databaseURL: "https://my-things-to-do-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "my-things-to-do",
+    storageBucket: "my-things-to-do.appspot.com",
+    messagingSenderId: "701583012637",
+    appId: "1:701583012637:web:091b338bd38e88f345a7f4",
+    measurementId: "G-TE1EGL3L0M"
+};
+// Initialize Firebase.
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let database = firebase.database();
+
+
+// Testing Firebase - will be deleted soon. 
+var bigOne = document.getElementById("bigOne");
+var dbRef = firebase.database().ref().child("text");
+dbRef.on("value", snap => bigOne.innerText = snap.val());
+
+// Disable buttons without functions.
 document.getElementById("edit-button").disabled = true;
 document.getElementById("sort-button").disabled = true;
-document.getElementById("delete-all-button").disabled = true;
-
-/*
-// Read input and create a new list item when clicking on the "Add new task" button.
-document.getElementById("add-button").addEventListener("click", () => {
-    const listItem = document.createElement("li");
-    const inputValue = document.getElementById("new-task").value;
-    const text = document.createTextNode(inputValue);
-    listItem.appendChild(text);
-    if (inputValue === '') {
-        alert("You must write what you want to do!");
-    } else {
-        document.getElementById("task-list").appendChild(listItem);
-    }
-    document.getElementById("new-task").value = "";
-
-// Add "trash can" icon at the end of task.
-    const span = document.createElement("span");
-    const deleteIcon = document.createElement("img");
-    deleteIcon.setAttribute('src', 'images/trash-can.png');
-    deleteIcon.className = "trash";
-    span.className = "close";
-    span.appendChild(deleteIcon);
-    listItem.appendChild(span);
-    
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-    const span2 = document.createElement("span");
-    const checkbox = document.createElement("INPUT");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.className = "checkbox";
-    span2.className = "check";
-    span2.appendChild(checkbox);
-    listItem.appendChild(span2);
-});
-*/
+//document.getElementById("delete-all-button").disabled = true;
 
 // Read input, send it to database and create a new list item when clicking on the "Add new task" button.
 document.getElementById("add-button").addEventListener("click", () => {
-    const listItem = document.createElement("li");
+    //const listItem = document.createElement("li");
     const inputValue = document.getElementById("new-task").value;
     
     if (inputValue === '') {
         alert("You must write what you want to do!");
     } else {
-        let task = {
-            todo: inputValue,
+        addItemsToList(inputValue);
+    } 
+
+});
+
+const addItemsToList = (inputValue) => {
+    const listItem = document.createElement("li");
+    let task = {
+            title: inputValue,
             done: false
         };    
-        firebase.database().ref("my_todos/").push(task);
-        listItem.id = task.key;
+        database.ref("my_todos/").push(task);
+        listItem.id = task.key;      
+        listItem.innerHTML = task.title;
+        
         document.getElementById("task-list").appendChild(listItem);
         document.getElementById("new-task").value = "";
-    } 
     
     // Add "trash can" icon at the end of task.
     const span = document.createElement("span");
@@ -82,8 +74,7 @@ document.getElementById("add-button").addEventListener("click", () => {
     span2.className = "check";
     span2.appendChild(checkbox);
     listItem.appendChild(span2);
-
-});
+};
 
 
 // Click on a "trush can" icon to delete item from the list.
@@ -99,7 +90,7 @@ for (i = 0; i < close.length; i++) {
 // Add "checked" sign and "line-through" when clicking on a list item.
 document.querySelector(".list").addEventListener("click",(event)  => {
     if (event.target.tagName === "LI") {
-        event.target.classList.toggle("checked");
+        event.target.classList.toggle("checked");     
     }
 });
 
@@ -111,25 +102,21 @@ document.querySelector(".input").addEventListener("keyup", (event) => {
     }
 });
 
+document.getElementById("delete-all-button").addEventListener("click", () => {
+    database.ref("my_todos/").remove();
+});
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyB5kRc09ifanS9_tXPoOAPh2OYcYGZibM8",
-    authDomain: "my-things-to-do.firebaseapp.com",
-    databaseURL: "https://my-things-to-do-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "my-things-to-do",
-    storageBucket: "my-things-to-do.appspot.com",
-    messagingSenderId: "701583012637",
-    appId: "1:701583012637:web:091b338bd38e88f345a7f4",
-    measurementId: "G-TE1EGL3L0M"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-let database = firebase.database();
+function fetchAllData(){
+    database.ref("my_todos/").once("value", function(snapshot){
+        snapshot.forEach(
+            function(ChildSnapshot){
+                let todoTask = ChildSnapshot.val().title;
+                addItemsToList(todoTask);
+            }
+        )
+    })
+}
 
-var bigOne = document.getElementById("bigOne");
-var dbRef = firebase.database().ref().child("text");
-dbRef.on("value", snap => bigOne.innerText = snap.val());
+window.onload(fetchAllData());
+
